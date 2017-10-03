67e9ce43bcdd067a839729ef4f998f665482512d
d-smith <doug.smith.mail@gmail.com>
2017-10-03 16:48:43 -0600

    Added replcate decision logic

--- Replicator.py
+++ Replicator.py
@@ -1,3 +1,13 @@
 def lambda_handler(event, context):
     print 'event: {}'.format(event)
     print 'context: {}'.format(context)
+    
+    event_records = event['Records']
+    for r in event_records:
+        print 'event name: {}'.format(r['eventName'])
+        
+        if r['eventName'] != 'REMOVE' and not ('replicate' in r['dynamodb']['NewImage']):
+            print '--> replication not indicated'
+            continue
+        
+        print '--> replicate'
