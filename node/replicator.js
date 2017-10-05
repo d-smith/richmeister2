feecd4c5ee1c300ff08841b8587fb95d180e559c
d-smith <doug.smith.mail@gmail.com>
2017-10-05 10:21:12 -0700

    More structure

--- node/replicator.js
+++ node/replicator.js
@@ -8,19 +8,53 @@ const handleRecord = (record,callback) => {
     
     if(record.eventName != 'REMOVE' && record.dynamodb.NewImage.replicate == undefined) {
             console.log('Replication not indicated',record.dynamodb.Keys)
+            checkDone(context);
     } else {
         console.log('replicate');
+        replicateRecord(record, callback);
     }
     
+    
+}
+
+const allProcessed = (callback) => {
+    console.log('done processing');
+    callback(null, 'yeah ok');
+}
+
+const checkDone = (callback) => {
     processed += 1;
     if(processed == numRecords) {
         allProcessed(callback);
     }
 }
 
-const allProcessed = (callback) => {
-    console.log('done processing');
-    callback(null, 'yeah ok');
+const replicateRecord = (record, callback) => {
+    if (record.eventName == 'INSERT') {
+        doInsert(record, callback);
+    } else if (record.eventName == 'MODIFY') {
+        doModify(record, callback);
+    } else  if (record.eventName == 'REMOVE') {
+        doRemove(record, callback);
+    } else {
+        console.log('unknown opcode: ' + record.eventName);
+        checkDone(callback);
+    }
+}
+
+const doInsert = (record, callback) => {
+    console.log('replicate insert');
+    checkDone(callback);
+}
+
+const doModify = (record, callback) => {
+    console.log('replicate modify');
+    checkDone(callback);
+}
+
+const doRemove = (record, callback) => {
+    console.log('replicate remove');
+    checkDone(callback);
 }
 
 exports.handler = (event, context, callback) => {
